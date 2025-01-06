fc8d7663b8f5e17d2b8bacf2ad31d34ae464f763
LichtAD <antu.dutta@g.bracu.ac.bd>
2025-01-06 22:07:30 +0600

    chnaged logo width

--- src/components/main/Navbar.jsx
+++ src/components/main/Navbar.jsx
@@ -70,7 +70,7 @@ const Navbar = () => {
                     <div className='flex gap-2 items-center'>
                         <NavLink to="/">
                             {/* <img src="/images/logo.png" className='w-16' alt="" /> */}
-                            <img src={theme === 'dark' ? "/images/logo-reverse.png" : "/images/logo.png"} className='w-24' alt="" />
+                            <img src={theme === 'dark' ? "/images/logo-reverse.png" : "/images/logo.png"} className='w-16' alt="" />
                         </NavLink>
                         <NavLink to="/" className="text-xl font-bold">Equisports</NavLink>
                         {/* <h2>
